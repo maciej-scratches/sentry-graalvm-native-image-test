fbedc5a5b072e2b495d9bdb057761ff70d1db4ef
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-07-14 12:36:13 +0200

    Add Sentry

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import io.sentry.DiagnosticLogger;
+//import io.sentry.GsonSerializer;
+import io.sentry.Sentry;
+import io.sentry.SystemOutLogger;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        Sentry.init(options -> {
+            options.setDebug(true);
+            options.setDsn("https://502f25099c204a2fbf4cb16edc5975d1@o447951.ingest.sentry.io/5428563");
+        });
+
+        Sentry.captureMessage("hello world");
     }
 }
